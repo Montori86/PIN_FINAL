834c1e4ebc35b6a507137ec9e356f0dd86d06b46
German <powit10@MacBook-Pro-de-PowIT10.local>
2025-02-27 16:47:20 -0300

    adding actions

--- 01_eksctl/create-cluster.sh
+++ 01_eksctl/create-cluster.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Variables
-CLUSTER_NAME=mundoes-cluster-G6
-AWS_REGION=us-east-2
+CLUSTER_NAME=eks-mundos-e
+AWS_REGION=us-east-1
+
+# Asegurarse de que /usr/local/bin está en el PATH
+export PATH=$PATH:/usr/local/bin
+
+# Verificar si eksctl está disponible
+if ! command -v eksctl &> /dev/null
+then
+    echo "Error: eksctl no está instalado o no está en el PATH"
+    exit 1
+fi
 
 # Set AWS credentials 
 aws sts get-caller-identity >> /dev/null
@@ -10,26 +20,27 @@ if [ $? -eq 0 ]
 then
   echo "Credenciales testeadas, proceder con la creacion de cluster."
 
-  # Creacion de cluster
+  # Creación del cluster en EKS
   eksctl create cluster \
-  --name $CLUSTER_NAME \
-  --region $AWS_REGION \
-  --nodes 3 \
-  --node-type t2.small \
-  --with-oidc \
-  --ssh-access \
-  --ssh-public-key jenkins \
-  --managed \
-  --full-ecr-access \
-  --zones us-east-2a,us-east-2b,us-east-2c
+    --name "$CLUSTER_NAME" \
+    --region us-east-1 \
+    --nodes 3 \
+    --node-type t3.small \
+    --with-oidc \
+    --ssh-access \
+    --ssh-public-key pin-grupo10 \
+    --managed \
+    --full-ecr-access \
+    --zones us-east-1a,us-east-1b,us-east-1c
 
-  if [ $? -eq 0 ]
-  then
-    echo "Cluster Setup Completo con eksctl ."
+  if [ $? -eq 0 ]; then
+    echo "Cluster creado exitosamente con eksctl."
   else
-    echo "Cluster Setup Falló mientras se ejecuto eksctl."
+    echo "Error: La creación del cluster falló al ejecutar eksctl"
+    exit 1
   fi
 else
-  echo "Please run aws configure & set right credentials."
-  echo "Cluster setup failed."
+  echo "No se encuentran credenciales de AWS configuradas. Por favor, ejecuta 'aws configure' para configurar las credenciales adecuadas"
+  echo "El setup del cluster ha fallado."
+  exit 1
 fi
