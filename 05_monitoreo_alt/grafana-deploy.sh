a98a3fe1e624a0e854d92f67e8e265293fbe3560
MauroPereira <mauro.a.pereira@gmail.com>
2025-03-04 17:39:42 -0300

    done: agregado nodegroup al script de grafana

---
+++ 05_monitoreo_alt/grafana-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Definir variables
+SCRIPT_DIR=$(dirname "$0")
+GRAFANA_VALUES="$SCRIPT_DIR/grafana.yml"
+ADMIN_PASSWORD="grupo-02"
+NODEGROUP_NAME="ng-grupo-2-grafana"
+
+# Crear el namespace para Grafana
+kubectl create namespace grafana
+
+# Instalar Grafana usando Helm y el archivo de configuración local
+helm install grafana grafana/grafana \
+    --namespace grafana \
+    --set persistence.storageClassName="gp3" \
+    --set persistence.enabled=true \
+    --set adminPassword="$ADMIN_PASSWORD" \
+    --set nodeGroupName="$NODEGROUP_NAME" \
+    --values "$GRAFANA_VALUES" \
+    --set service.type=LoadBalancer
