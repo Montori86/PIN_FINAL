1056045a37f52c42eb453f257872a4ba04b8ae9f
MauroPereira <mauro.a.pereira@gmail.com>
2025-03-04 18:41:36 -0300

    Commit anterior

--- 05_monitoreo_alt/grafana-deploy.sh
+++ 05_monitoreo_alt/grafana-deploy.sh
@@ -27,7 +27,7 @@ helm install grafana grafana/grafana \
     --values "$GRAFANA_VALUES" \
     --set service.type=LoadBalancer
 
-# NOTE: el cambio de gp2 a gp3-immediate se hace para que la StorageClass este configurada para provisionar volúmenes de tipo "gp3" en AWS con la opción "Immediate", 
-# lo que significa que el volumen se provisiona de inmediato sin esperar a que se consuma, lo que mejora el tiempo de provisión.
+# NOTE: el cambio de gp2 a gp3-immediate se hace para que la StorageClass este configurada para provisionar volúmenes de tipo "gp3" en AWS con la 
+# opción "Immediate", lo que significa que el volumen se provisiona de inmediato sin esperar a que se consuma, lo que mejora el tiempo de provisión.
 
 
