a0ab571845563bd971145ededc332c8aede197e1
MauroPereira <mauro.a.pereira@gmail.com>
2025-03-04 12:56:41 -0300

    done: cambiado de gp2 a gp3 (ver NOTE)

--- 04_monitoreo/prometheus-deploy.sh
+++ 04_monitoreo/prometheus-deploy.sh
@@ -15,8 +15,14 @@ kubectl create namespace prometheus
 
 helm install prometheus prometheus-community/prometheus \
 --namespace prometheus \
---set alertmanager.persistentVolume.storageClass="gp2" \
---set server.persistentVolume.storageClass="gp2"
+--set alertmanager.persistentVolume.storageClass="gp3" \
+--set server.persistentVolume.storageClass="gp3" 
+
+# NOTE: El cambio de storageclass de gp2 a gp3 se debe a que los nodos del clúster EKS ya están usando volúmenes gp3, 
+# por lo que es mejor mantener coherencia en todo el stack. gp3 ofrece mejor rendimiento y 
+# menor costo en comparación con gp2, sin depender del tamaño del volumen para IOPS y throughput.
+# Evitamos posibles problemas si la StorageClass gp2 no está disponible en el clúster, así garantizamos que tanto 
+# Prometheus como los nodos usen la misma tecnología de almacenamiento.
 
 # Verificar la instalación
 kubectl get all -n prometheus
