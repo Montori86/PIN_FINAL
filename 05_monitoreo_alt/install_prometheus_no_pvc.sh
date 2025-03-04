a6b00bca3a29e7ba3629c2bd05b5854bb09ef7c7
MauroPereira <mauro.a.pereira@gmail.com>
2025-03-04 17:33:49 -0300

    done: agregada documentacion en README.md y nuevo script para crear nodos de prometheus sin pvc

---
+++ 05_monitoreo_alt/install_prometheus_no_pvc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+NAMESPACE="prometheus"
+
+# Eliminar Prometheus si existe
+if helm ls -n "$NAMESPACE" | grep -q prometheus; then
+  echo "🗑 Eliminando Prometheus..."
+  helm uninstall prometheus -n "$NAMESPACE"
+else
+  echo "⚠️ Prometheus ya estaba eliminado."
+fi
+
+# Asegurar que el namespace no existe
+if kubectl get namespace "$NAMESPACE" &>/dev/null; then
+  echo "🗑 Eliminando namespace $NAMESPACE..."
+  kubectl delete namespace "$NAMESPACE"
+else
+  echo "⚠️ Namespace $NAMESPACE ya estaba eliminado."
+fi
+
+# Crear namespace
+kubectl create namespace "$NAMESPACE"
+
+# Instalar Prometheus sin almacenamiento persistente
+helm install prometheus prometheus-community/prometheus \
+  --namespace "$NAMESPACE" \
+  --set alertmanager.persistentVolume.enabled=false \
+  --set server.persistentVolume.enabled=false
+
+echo "✅ Instalación de Prometheus completada sin volúmenes persistentes."
