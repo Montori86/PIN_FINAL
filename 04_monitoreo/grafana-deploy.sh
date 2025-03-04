949c0fc1fd16525eb1e2e0b4aea31e9e3c673d53
MauroPereira <mauro.a.pereira@gmail.com>
2025-03-03 23:19:27 -0300

    done: modificado grafana-deploy.sh para que cargue automaticamente el grafana.yml

--- 04_monitoreo/grafana-deploy.sh
+++ 04_monitoreo/grafana-deploy.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+# Definir variables
+SCRIPT_DIR=$(dirname "$0")
+GRAFANA_VALUES="$SCRIPT_DIR/grafana.yml"
+ADMIN_PASSWORD="grupo-02"
+
+# Crear el namespace para Grafana
 kubectl create namespace grafana
 
+# Instalar Grafana usando Helm y el archivo de configuración local
 helm install grafana grafana/grafana \
     --namespace grafana \
     --set persistence.storageClassName="gp2" \
     --set persistence.enabled=true \
-    --set adminPassword='EKS!sAWSome' \
-    --values ${HOME}/environment/grafana/grafana.yaml \
+    --set adminPassword="$ADMIN_PASSWORD" \
+    --values "$GRAFANA_VALUES" \
     --set service.type=LoadBalancer
